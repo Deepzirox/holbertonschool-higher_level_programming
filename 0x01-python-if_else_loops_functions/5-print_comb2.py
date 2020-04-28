36fd2f38c8bd7a691225313ad257566a1cbb34d3
deep <deep@localhost.localdomain>
2020-04-28 22:58:34 +0000

    fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@
 
 for num in range(0, 100):
     if num != 99:
-        print("{0:02d}, ".format(num), end = '')
+        print("{0:02d}, ".format(num), end='')
     else:
         print("{0:02d}".format(num))
