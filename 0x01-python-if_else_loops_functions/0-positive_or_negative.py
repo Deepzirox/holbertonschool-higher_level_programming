53f09d1bdff07812354bf00111a3ab9ed83717d1
deep <deep@localhost.localdomain>
2020-04-28 19:54:59 +0000

    fix

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import random
+
+
 number = random.randint(-10, 10)
 if number > 0:
     print("{:d} is positive".format(number))
@@ -7,4 +9,3 @@ elif number < 0:
     print("{:d} is negative".format(number))
 else:
     print("{:d} is zero".format(number))
-
