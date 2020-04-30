eaf8e16afc5da4eb5ecc472007af16ede47ab03e
deep <deep@localhost.localdomain>
2020-04-30 23:58:08 +0000

    add

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,9 +3,9 @@ import hidden_4
 
 
 if __name__ == '__main__':
-	names = dir(hidden_4)
-	for i in names:
-		if i[0] == '_':
-			pass
-		else:
-			print(i)
+    name = dir(hidden_4)
+    for n in name:
+        if n[0] == '_':
+            pass
+        else:
+            print(n)
