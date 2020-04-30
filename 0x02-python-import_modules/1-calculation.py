fe2f05f8569be24ae3ca16fa5a81b117f8d20536
deep <deep@localhost.localdomain>
2020-04-30 18:49:02 +0000

    fixing trailing whitespace

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ import calculator_1
 
 
 a = 10
-b = 5    
+b = 5
 print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
 print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
 print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
