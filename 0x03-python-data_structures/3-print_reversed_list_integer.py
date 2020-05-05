c4f222b93e1f923bb0bd53dc6592c5cd335f8820
Camilo tobon <1611@holbertonschool.com>
2020-05-05 17:33:35 -0500

    add

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 1:
-        print("{:d}".format(re))
-    else:
-        for re in my_list[::-1]:
-            print("{:d}".format(re))
+    a = my_list
+    a.reverse()
+    for x in a:
+        print("{:d}".format(x))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new = []
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return [val for val in my_list]
     else:
         new = [val for val in my_list]
