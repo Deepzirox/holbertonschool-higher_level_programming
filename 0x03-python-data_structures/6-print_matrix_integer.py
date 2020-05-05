3f3ab539b564175fbd5fa2e9bd12440a7ea0552b
Camilo tobon <1611@holbertonschool.com>
2020-05-05 12:22:27 -0500

    add

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         h = len(row)
         c = 0
         for col in row:
-            print(col, end='')
+            print("{:d}".format(col), end='')
             if c != h - 1:
                 print(' ', end='')
             c += 1
