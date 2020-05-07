303d1665e0251c98d73a55ce052b86afc4c39557
Camilo tobon <1611@holbertonschool.com>
2020-05-07 14:22:29 -0500

    fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if search < len(my_list)
+    if search < len(my_list):
         new = [i for i in my_list]
         new[search - 1] = replace
         return new
