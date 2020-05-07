a100d7dc06536d3a0c084b9424188c46d331895d
Camilo tobon <1611@holbertonschool.com>
2020-05-07 17:29:08 -0500

    add

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or len(a_dictionary) == 0:
         return None
     max_score = max(a_dictionary.values())
     keys = a_dictionary.keys()
     for key in keys:
         if a_dictionary[key] == max_score:
             return key
+
+
+a_dictionary = {}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
