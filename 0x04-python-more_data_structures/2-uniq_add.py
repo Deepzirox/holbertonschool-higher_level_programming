af3226c68ec9ececbea02e21e5311f837d2a2f96
Camilo tobon <1611@holbertonschool.com>
2020-05-07 15:13:46 -0500

    add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -6,11 +6,8 @@ def uniq_add(my_list=[]):
     for i in my_list:
         if i in tmp:
             ocurrence = True
-        
-        if ocurrence != True:
+        if ocurrence is not True:
             res += i
-
         ocurrence = False
         tmp.append(i)
-        
     return res
