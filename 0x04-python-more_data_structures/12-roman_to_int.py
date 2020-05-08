7cdd8f8b64f174d896815024de76713f6e7d1fc4
Camilo tobon <1611@holbertonschool.com>
2020-05-07 19:41:56 -0500

    add

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     base = {
-        "I" : 1,
-        "V" : 5,
-        "X" : 10,
-        "L" : 50,
-        "C" : 100,
-        "D" : 500,
-        "M" : 1000
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
-    if not isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    if roman_string.isnumeric == True:
+    if roman_string.isnumeric is True:
         return 0
     sub = False
     res = 0
@@ -20,7 +20,7 @@ def roman_to_int(roman_string):
         if i == "I":
             sub = True
         if sub and i != "I":
-            res += base.get(i) - 2
+            res -= (base.get(i) - 1)
             sub = False
         else:
             res += base.get(i)
