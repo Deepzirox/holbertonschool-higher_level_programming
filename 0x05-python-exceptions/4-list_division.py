dc9397ddd52a0e1ddb6f1da54d7348c479661e2e
Camilo tobon <1611@holbertonschool.com>
2020-05-18 17:29:31 -0500

    add

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+def list_division(my_list_1, my_list_2, list_length):
+    res = []
+    div = 0
+    for n in range(list_length):
+        try:
+            div = my_list_1[n] / my_list_2[n]
+            res.append(div)
+        except ZeroDivisionError:
+            print("{}".format("division by 0"))
+            res.append(0)
+        except TypeError:
+            print("{}".format("wrong type"))
+            res.append(0)
+        except IndexError:
+            print("{}".format("out of range"))
+            res.append(0)
+    return res
