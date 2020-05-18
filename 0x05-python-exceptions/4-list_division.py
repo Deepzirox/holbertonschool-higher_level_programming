b234ec69055f97f2e282200cdd07c56f3a353c46
Camilo tobon <1611@holbertonschool.com>
2020-05-18 17:32:52 -0500

    fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,4 +15,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("{}".format("out of range"))
             res.append(0)
+        finally:
+            continue
     return res
