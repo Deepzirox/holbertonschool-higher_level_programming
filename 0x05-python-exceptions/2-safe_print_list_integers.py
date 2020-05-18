c44955746ed1ac39054ed431df4f4fa3e0487b59
Camilo tobon <1611@holbertonschool.com>
2020-05-18 16:05:54 -0500

    fixing

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,5 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
             pass
         except TypeError:
             pass
+        except IndexError:
+            pass
     print(' ')
     return intc
