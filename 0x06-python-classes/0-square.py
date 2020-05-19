154769c0cd152318672cdcdc34a72a90b8255e5a
Camilo tobon <1611@holbertonschool.com>
2020-05-19 17:03:41 -0500

    fixing

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,10 +2,9 @@
 ''' 0-Square
     Create - class for create and instance of a class
 '''
-class Square:
-    '''
-    Square - Start Square
-    @Atributtes: 0
-    '''
-    def __init__(self):
-        pass
+
+
+class Square: 
+    'Empty class'
+    pass
+
