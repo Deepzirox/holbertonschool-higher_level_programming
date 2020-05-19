ee147c667da5562956dd602b3275532701d14881
Camilo tobon <1611@holbertonschool.com>
2020-05-19 17:01:02 -0500

    f

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,7 +16,7 @@ class Square:
     '''
     def __init__(self, size=0):
         try:
-            if size < 0:
+            if size <= 0:
                 raise Exception("size must be >= 0")
         except TypeError:
             raise Exception("size must be an integer")
