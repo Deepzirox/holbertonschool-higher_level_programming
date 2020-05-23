cf59198ecc9fc73b532cefab93b5eb16afd06cb7
Camilo tobon <1611@holbertonschool.com>
2020-05-22 22:11:11 -0500

    adding file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,9 +1,18 @@
 import unittest
+"""[Unittest for max_integer]
+"""
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """[Test for max integet function]
+
+    Arguments:
+        unittest {[testing]} -- module for use unittest.testcases
+    """
     def test_results(self):
+        """[test_results]
+        """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
         self.assertEqual(max_integer([1]), 1)
@@ -11,5 +20,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([0]), 0)
 
     def test_fails(self):
+        """[test_fails]
+        """
         self.assertRaises(TypeError, max_integer, 123)
         self.assertRaises(TypeError, max_integer, True)
