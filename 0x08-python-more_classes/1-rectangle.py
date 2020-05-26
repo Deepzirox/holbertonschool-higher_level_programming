b7b502a9f31736549a49f5c0abf9104c4cc5d817
Camilo tobon <1611@holbertonschool.com>
2020-05-25 19:32:06 -0500

    f

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         if type(value) not in [int]:
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("ValueError")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -34,6 +34,6 @@ class Rectangle:
         if type(value) not in [int]:
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("ValueError")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
