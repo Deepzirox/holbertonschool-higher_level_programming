99760165af1f6384279f9c9ee2856f90184344be
Camilo tobon <1611@holbertonschool.com>
2020-06-02 18:50:32 -0500

    add

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,13 @@
 """
 
 def inherits_from(obj, a_class):
+    """[summary]
+
+    Arguments:
+        obj {[type]} -- [description]
+        a_class {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """    
     return not issubclass(a_class, type(obj))
