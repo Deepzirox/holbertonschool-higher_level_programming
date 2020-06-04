654654daf0b08a62acf217073af8258eaac7def3
Camilo tobon <1611@holbertonschool.com>
2020-06-04 11:47:38 -0500

    add

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
+"""[summary]
+"""
 
 
 def read_lines(filename="", nb_lines=0):
+    """[summary]
+
+    Args:
+        filename (str, optional): [description]. Defaults to "".
+        nb_lines (int, optional): [description]. Defaults to 0.
+    """    
     with open(filename) as file:
         data = file.read()
-        jumps = 0
-        for i in data:
-            if i == '\n':
-                jumps += 1
-            if jumps == nb_lines:
-                break
-            print(i, end='')
-            
-
-
-print("1 line:")
-read_lines("my_file_0.txt", 1)
-print("--")
-print("3 lines:")
-read_lines("my_file_0.txt", 3)
-print("--")
-print("Full content:")
-read_lines("my_file_0.txt")
+        if nb_lines <= 0:
+            print(data, end='')
+        else:
+            jumps = 0
+            for i in data:
+                if i == '\n':
+                    print()
+                    jumps += 1
+                    continue
+                if jumps == nb_lines:
+                    break
+                print(i, end='')
