62a24cfe0850a3603f0c4ef8562b59592b7072a8
Camilo tobon <1611@holbertonschool.com>
2020-06-09 17:04:47 -0500

    dd

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,8 +37,7 @@ class Base:
         Returns:
             [type]: [description]
         """
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
-            return "[]"
+        
         new_json_array = []
         for dictionary in list_dictionaries:
             new_json_array.append(dictionary)
