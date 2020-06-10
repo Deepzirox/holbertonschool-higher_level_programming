8167ff0439515319d36b454b83581a8c460f80d2
Camilo tobon <1611@holbertonschool.com>
2020-06-09 21:22:26 -0500

    add

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,9 +1,18 @@
+#!/usr/bin/python3
 from models.base import Base
 from models.rectangle import Rectangle
 import json
 import unittest
 
+"""
+testing
+"""
+
+
 class TestBase(unittest.TestCase):
+    """
+    Testing class
+    """
     def test_base1(self):
         x = Base()
         self.assertEqual(x.id, 1)
@@ -15,5 +24,3 @@ class TestBase(unittest.TestCase):
     def test_base3(self):
         x2 = Base()
         self.assertEqual(x2.id, 2)
-
-    
