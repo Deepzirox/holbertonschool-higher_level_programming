0d40c39952ce7d2a631b23074069547d01940dd9
Camilo Tobon <1611@holbertonschool.com>
2020-07-13 22:12:42 -0500

    adding advanced task 102

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Select top 3 hottest cities during July and August
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month BETWEEN 7 AND 8  GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
