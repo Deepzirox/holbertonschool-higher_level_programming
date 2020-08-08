f9c2f461e079d3e9a1366e33a862ca1291c67a46
Camilo Tobon <1611@holbertonschool.com>
2020-08-08 14:18:49 -0500

    fixing n ines

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect
+    p = 3306
+    h = "localhost"
+    db = db(host=h, port=p, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states;")
     row = cursor.fetchall()
