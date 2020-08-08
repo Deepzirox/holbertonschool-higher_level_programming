865fa5a9afc54d8c7ac6250b4565cdf6a90a7946
Camilo Tobon <1611@holbertonschool.com>
2020-08-08 15:20:45 -0500

    f2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,9 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect
     p = 3306
     h = "localhost"
+    db = MySQLdb.connect
     db = db(host=h, port=p, user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states;")
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-import MySQLdb
-from sys import argv
 
 '''
 Get states by input
 '''
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%';")
-rows = cursor.fetchall()
-for r in rows:
-    print(r)
+import MySQLdb
+from sys import argv
 
 
-db.close()
+if __name__ == '__main__':
+    p = 3306
+    h = "localhost"
+    db = MySQLdb.connect
+    db = db(host=h, port=p, user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%';")
+    rows = cursor.fetchall()
+    for r in rows:
+        print(r)
+    db.close()
