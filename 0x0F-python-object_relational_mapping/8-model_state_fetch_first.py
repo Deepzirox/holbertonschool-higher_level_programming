d1c919999296c7a803fafa99c3cefb9a867e9517
Camilo Tobon <1611@holbertonschool.com>
2020-08-09 02:27:21 -0500

    add

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,5 +15,8 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(states.id, states.name))
+    try:
+        states = session.query(State).order_by(State.id).first()
+        print(states)
+    except:
+        print()
