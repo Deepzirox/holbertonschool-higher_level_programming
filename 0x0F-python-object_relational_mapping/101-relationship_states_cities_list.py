14dc01be7821186345cb05de68a6cc895cc81f1e
Camilo Tobon <1611@holbertonschool.com>
2020-08-09 22:54:27 -0500

    please

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -4,22 +4,26 @@
 Get all states using sql alchemy
 '''
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import (create_engine, func)
 from sqlalchemy.orm import sessionmaker
 import sys
 from model_state import Base as base_state, State
 from model_city import Base as base_city, City
-
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    for state in session.query(State).all():
-        print("{}: {}".format(state.id, state.name))
-        for city in session.query(City).filter(state.id == City.state_id):
-            print("\t{}: {}".format(city.id, city.name))
+    query = session.query(City, State).filter(City.state_id == State.id).all()
+    curr = 0
+    for ct, st in query:
+        if st.name == curr:
+            print("\t{}: {}".format(ct.id, ct.name))
+            continue
+        else:
+            print("{}: {}".format(st.id, st.name))
+            curr = st.name
+        print("\t{}: {}".format(ct.id, ct.name))
 
     session.close()
