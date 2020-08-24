55d5876876755f0ab10c689ad603a45090dccc03
Camilo Tobon <1611@holbertonschool.com>
2020-08-24 17:12:39 -0500

    adding tasks

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" OK """
+
+import urllib.request
+import sys
+
+
+
+def get_request_id():
+	url = sys.argv[1]
+	with urllib.request.urlopen(url) as res:
+		headers = res.headers
+		print(headers.get("X-Request-Id"))
+
+
+if __name__ == '__main__':
+	get_request_id()
