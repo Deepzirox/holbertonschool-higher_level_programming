379dab088757c57d930f189846b7f25db2fee0e0
Camilo Tobon <1611@holbertonschool.com>
2020-08-24 17:14:49 -0500

    adding tasks

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,14 +6,13 @@ import urllib.request
 import sys
 
 
-
 def get_request_id():
-	""" GET REQUEST ID FROM HEADER """
-	url = sys.argv[1]
-	with urllib.request.urlopen(url) as res:
-		headers = res.headers
-		print(headers.get("X-Request-Id"))
+    """ GET REQUEST ID FROM HEADER """
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as res:
+        headers = res.headers
+        print(headers.get("X-Request-Id"))
 
 
 if __name__ == '__main__':
-	get_request_id()
+    get_request_id()
