00b1ef9ea8a8b3145a2bc467f0e7cf7d5f740bd3
Camilo Tobon <1611@holbertonschool.com>
2020-08-25 00:03:52 -0500

    adding tasks

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,10 +8,11 @@ import sys
 
 
 def post_email():
-	res = requests.post(argv[1], data={'email': argv[2]})
-	print(res.text)
+    """ Post fucking email """
+    res = requests.post(argv[1], data={'email': argv[2]})
+    print(res.text)
 
 
 
 if __name__ == '__main__':
-	post_email()
+    post_email()
