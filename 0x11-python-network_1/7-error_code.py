112d60cba9872e6fb018dbd4761c775d6e072e37
Camilo Tobon <1611@holbertonschool.com>
2020-08-25 00:25:22 -0500

    adding tasks

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,13 +7,13 @@ import requests
 
 
 def error_code():
-	""" ok """
-	re = requests.get(sys.argv[1])
-	if re.status_code >= 400:
-		print("Error code: {}".format(re.status_code))
-	else:
-		print(re.text)
+    """ ok """
+    re = requests.get(sys.argv[1])
+    if re.status_code >= 400:
+        print("Error code: {}".format(re.status_code))
+    else:
+        print(re.text)
 
 
 if __name__ == '__main__':
-	error_code()
+    error_code()
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" Ok """
+
+from sys import argv
+import requests
+
+
+def search():
+    """ ok """
+    v = argv[1]
+    if len(argv) < 2:
+        v = ""
+
+    re = requests.post(
+        'http://0.0.0.0:5000/search_user', data={'q': v})
+
+    try:
+        data = re.json()
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    search()
