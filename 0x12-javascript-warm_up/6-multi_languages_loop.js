b3f99184dbf02dbc8f0bb68a6d234ecee76630d0
Camilo <1611@holbertonschool.com>
2020-09-15 21:03:23 -0500

    add

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < list.length; i++) {
-  console.log(myArray[i]);
+  console.log(list[i]);
 }
