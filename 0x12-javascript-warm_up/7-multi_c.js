54e890b405cce7f0f88436ee3204c733f866f138
Camilo <1611@holbertonschool.com>
2020-09-15 21:07:17 -0500

    f

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let n = parseInt(process.argv[2]);
+const n = parseInt(process.argv[2]);
 if (isNaN(n)) {
   console.log('Missing number of occurrences');
 } else {
-    while(n--){
-        console.log("C is fun")
-    }
+  for (let i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
 }
