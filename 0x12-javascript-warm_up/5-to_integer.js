f9618a576640fa28693c542a4be4e7c1455bec9d
Camilo <1611@holbertonschool.com>
2020-09-15 20:57:55 -0500

    add

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2]))) {
+const n = parseInt(process.argv[2]);
+if (isNaN(n)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + number);
+  console.log('My number: ' + n);
 }
